513adadd2d6415c2f7962a24a65cdd16e1d763dc
litmaspatra <139606391+litmaspatra@users.noreply.github.com>
2026-09-23 15:46:01 +0530

    Check for app updates on launch

--- app/src/main/java/com/knowyourcase/app/MainActivity.kt
+++ app/src/main/java/com/knowyourcase/app/MainActivity.kt
@@ -8,11 +8,29 @@ import com.knowyourcase.app.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var updateManager: AppUpdateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        updateManager = AppUpdateManager(this, this)
+        updateManager.checkForUpdate(silent = true)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::updateManager.isInitialized) {
+            updateManager.retryPendingInstall()
+        }
+    }
+
+    override fun onDestroy() {
+        if (::updateManager.isInitialized) {
+            updateManager.close()
+        }
+        super.onDestroy()
     }
 
     override fun onSupportNavigateUp(): Boolean {
